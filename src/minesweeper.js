43b646d876afb77f8e48bb94aa3a39bd34a2a1ee
mzlovelyj <liljp064@gmail.com>
2017-10-08 20:58:12 -0400

    hardcoding the board with the Bombs and guesses and empty spots.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,23 @@
 const blankLine = '  |   |  ';
+//hardCoded const for blankLine which is the start of the empty board.
 console.log('This is what an empty board would look like:');
 console.log(blankLine);
 console.log(blankLine);
 console.log(blankLine);
+//I console.log to view an empty board. 
+
+const guessLine = '1 |   |  ';
+/* hardCode const for a guessLine 
+represents what the board will look like when a player guesses by "clicking"
+(selecting) the first square of this row.*/
+
+const bombLine = '  | B |  ';
+/* hardCode const for a bombLine 
+bombLine represents what the board will look like when a player clicks and reveals a bomb.*/
+
+
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+//I console.log to view a board with guessLine,bombLine & blankLine.
