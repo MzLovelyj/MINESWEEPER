60cc54857569f73644b92e6e0ceda2b249b6e385
mzlovelyj <liljp064@gmail.com>
2017-10-27 16:19:17 -0400

    updating board, CodeAcadmy Exercise

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,23 +1,26 @@
-const blankLine = '  |   |  ';
-//hardCoded const for blankLine which is the start of the empty board.
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-//I console.log to view an empty board. 
-
-const guessLine = '1 |   |  ';
-/* hardCode const for a guessLine 
-represents what the board will look like when a player guesses by "clicking"
-(selecting) the first square of this row.*/
-
-const bombLine = '  | B |  ';
-/* hardCode const for a bombLine 
-bombLine represents what the board will look like when a player clicks and reveals a bomb.*/
+//npm run build to compile then do node src/minesweeper.js
+const printBoard = board => {
+    console.log("Current Board:  ");
+    // Log 'Current Board: '
+    // console.log(board[0]);
+    // Log the first element of the board variable
+    console.log(board[0].join('|'));
+    console.log(board[1].join('|'));
+    console.log(board[2].join('|'));
+};
 
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
-//I console.log to view a board with guessLine,bombLine & blankLine.
+
+const board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+];
+
+printBoard(board);
+//printing emptyBoard
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
+//printing Board with the 1 on the 1st row and 2nd element and 
+//the last row with the 3 rd element print B
